0cb4a1894838ea6299dd35fed8e8ff50c7d508c0
shariarhossain23e <shariarhossain23@gamil.comm>
2022-07-20 12:57:24 +0600

    first push

--- src/App.js
+++ src/App.js
@@ -4,12 +4,7 @@ function App() {
   return (
     <div>
       <h3 className="text-3xl text-center">Hello React</h3>
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Porro
-        distinctio corrupti, voluptates aliquid nobis perspiciatis reprehenderit
-        unde possimus nam nostrum esse voluptatum, molestias quibusdam
-        exercitationem laborum saepe et earum facere.
-      </p>
+      <h1>shariar</h1>
     </div>
   );
 }
