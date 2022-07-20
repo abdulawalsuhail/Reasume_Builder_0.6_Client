e0a5834d25bc6e4c5800e44317d4c74b0dd708de
Md.Abu Tahir Sarker <msarker141151@bscse.uiu.ac.bd>
2022-07-20 16:05:44 +0600

    color selection

--- src/Pages/Home/Hero.js
+++ src/Pages/Home/Hero.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Hero = () => {
     return (
         <div>
-            <h1>This is hero.</h1>
+            <h1 className='text-secondary'>This is hero.</h1>
         </div>
     );
 };
