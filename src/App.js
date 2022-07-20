67a3c6479a3269a47f5735ddccede2b7723f82ef
ifty03 <ashikulislamb84@gmail.com>
2022-07-20 17:13:38 +0600

    route added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import "./App.css";
 import Navbar from "./Components/Navbar/Navbar";
 import Home from "./Pages/Home/Home";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div>
       <Navbar />
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/home" element={<Home />}></Route>
+      </Routes>
     </div>
   );
 }
