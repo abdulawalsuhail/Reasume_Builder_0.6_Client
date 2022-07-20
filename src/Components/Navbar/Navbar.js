10034062d8ef6d8ad4cf1e91b67b89fcc77a8341
Md.Abu Tahir Sarker <msarker141151@bscse.uiu.ac.bd>
2022-07-20 17:32:17 +0600

    Navigation bar complete

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,9 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Navbar = () => {
+  const [user, setUser] = useState(true);
+  const navItem = <>
+            <li><a>FEATURE</a></li>
+            <li><a>RESUME</a></li>
+            <li><a>CV</a></li>
+            <li><a>COVER LETTER</a></li>
+            <li><a>CAREER COUNSELLING</a></li>
+            <li><a>PRICING</a></li>
+            <a class="btn btn-primary btn-outline btn-xs md:btn-sm mt-2">Create My Resume</a>
+            
+  </>
+
   return (
-    <div>
-      <h2>Hello navbar</h2>
+    <div className="mx-4">
+      {/* Navbar start source code from Daisy UI */}
+      <div class="navbar">
+        {/* Navbar left portion */}
+        <div class="navbar-start">
+          <div class="dropdown">
+            <label tabindex="0" class="btn btn-ghost lg:hidden">
+              <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+            </label>
+            <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+              {navItem}
+            </ul>
+          </div>
+          <a class="btn btn-ghost normal-case text-xl">Resume Builder</a>
+        </div>
+        {/* End Navbar left Portion */}
+
+        {/* Navbar center portion */}
+        <div class="navbar-center hidden lg:flex">
+          <ul class="menu menu-horizontal p-0">
+            {navItem}
+          </ul>
+        </div>
+        {/* End Navbar center portion */}
+
+        {/* Navbar end portion */}
+        <div class="navbar-end">
+          {
+            user
+              ? 
+              <div class="dropdown dropdown-end">
+                <label tabindex="0" class="btn btn-ghost btn-circle avatar">
+                  <div class="w-10 rounded-full">
+                    <img src="https://placeimg.com/80/80/people" />
+                  </div>
+                </label>
+                <ul tabindex="0" class="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52">
+                  <li>
+                    <a class="justify-between">
+                      Profile
+                      <span class="badge">New</span>
+                    </a>
+                  </li>
+                  <li><a>Settings</a></li>
+                  <li><a>Logout</a></li>
+                </ul>
+              </div>
+              :
+              <a class="btn btn-secondary btn-xs md:btn-sm mr-2 ">Login</a>
+          }
+        </div>
+        {/* End navbar end portion */}
+      </div>
     </div>
   );
 };
