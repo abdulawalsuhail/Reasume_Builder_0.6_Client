c43fcbdbbc487a0fc687a24583a34955402e0127
Sadnan-2019 <54290526+Sadnan-2019@users.noreply.github.com>
2022-07-21 18:23:06 +0600

    again pull

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-<<<<<<< HEAD
-=======
-import { Toaster } from "react-hot-toast";
-import { Route, Routes } from "react-router-dom";
->>>>>>> 8c6198fd08242cffc8e131f828c786b4afaf754a
 import "./App.css";
 import Navbar from "./Components/Navbar/Navbar";
 import Home from "./Pages/Home/Home";
-import SignIn from "./Shared/SignIn/SignIn";
+ 
 
 function App() {
   return (
     <div>
       <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/home" element={<Home />}></Route>
-        <Route path="/signup" element={<Signup></Signup>}></Route>
-      </Routes>
-      <Login />
-      <Footer />
-      <Toaster />
+      <Home />
+      {/* <SignIn></SignIn> */}
     </div>
   );
 }
