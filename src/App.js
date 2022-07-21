b14b4b3aabc71673a27512cb0d033961b4364a0c
Sadnan-2019 <54290526+Sadnan-2019@users.noreply.github.com>
2022-07-21 14:11:50 +0600

    new push

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Navbar from "./Components/Navbar/Navbar";
 import Home from "./Pages/Home/Home";
+import SignIn from "./Shared/SignIn/SignIn";
 
 function App() {
   return (
     <div>
       <Navbar />
       <Home />
+      {/* <SignIn></SignIn> */}
     </div>
   );
 }
---
+++ src/Shared/SignIn/SignIn.js
@@ -0,0 +1,53 @@
+import React from "react";
+
+const SignIn = () => {
+  return (
+    <div>
+      <div class="hero min-h-screen bg-base-200">
+        <div class="hero-content flex-col lg:flex-row-reverse">
+          <div class="card flex-shrink-0 w-full max-w-sm shadow-2xl  ">
+            <p className="text-2xl   p-3">
+              Create a resume you<br></br> are proud of
+            </p>
+
+            <div className="p-3 grid lg:grid-cols-2  gap-4 items-center justify-center      ">
+              <div>
+                <p className="text-2xl	">icon</p>
+              </div>
+              <div className="">
+                <p className="text-1xl	">Save time with hassle free templates</p>
+              </div>
+            </div>
+            <div className="p-3 grid lg:grid-cols-2  gap-4      ">
+              <div>
+                <p className="text-2xl	">icon</p>
+              </div>
+              <div>
+                <p className="text-1xl	">Save time with hassle free templates</p>
+              </div>
+            </div>
+            <div className="p-3 grid lg:grid-cols-2  gap-4      ">
+              <div>
+                <p className="text-2xl	">icon</p>
+              </div>
+              <div>
+                <p className="text-1xl	">Save time with hassle free templates</p>
+              </div>
+            </div>
+            <div className="p-3 grid lg:grid-cols-2  gap-4      ">
+              <div>
+                <p className="text-2xl	">icon</p>
+              </div>
+              <div>
+                <p className="text-1xl	">Save time with hassle free templates</p>
+              </div>
+            </div>
+            <p className="p-3">Get inspired by <a href=""> 200+ Free Resume Examples and Template</a></p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SignIn;
