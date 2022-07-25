7d7aebc15ed444e90fe6c553d1aca98d413ad75d
Md.Abu Tahir Sarker <msarker141151@bscse.uiu.ac.bd>
2022-07-25 17:41:36 +0600

    Template section updated

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ import Login from "./Components/Navbar/Login";
 import Navbar from "./Components/Navbar/Navbar";
 import Signup from "./Components/Navbar/Signup";
 import Home from "./Pages/Home/Home";
+import CoverLetter from "./Pages/Home/ResumeTemplate/CoverLetter";
+import Cv from "./Pages/Home/ResumeTemplate/Cv";
+import Resume from "./Pages/Home/ResumeTemplate/Resume";
+import ResumeTemplate from "./Pages/Home/ResumeTemplate/ResumeTemplate";
 import Footer from "./Shared/Footer/Footer";
 
 function App() {
@@ -12,10 +16,15 @@ function App() {
     <div>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />}></Route>
+        <Route path="/" element={<Home />}>
+          <Route index element={<Resume />}></Route>
+          <Route path="cv" element={<Cv />}></Route>
+          <Route path="coverLetter" element={<CoverLetter/>}></Route>
+        </Route>
         <Route path="/home" element={<Home />}></Route>
         <Route path="/signup" element={<Signup></Signup>}></Route>
         <Route path="/login" element={<Login />}></Route>
+        <Route path="resumeTemplate" element={<ResumeTemplate />}/>
       </Routes>
       <Footer />
       <Toaster />
--- src/Pages/Home/ResumeTemplate/ResumeTemplate.js
+++ src/Pages/Home/ResumeTemplate/ResumeTemplate.js
@@ -2,87 +2,24 @@ import React from "react";
 import "./ResumeTemplate.css";
 import template_1 from "../../../assets/demo1.png";
 import template_2 from "../../../assets/demo2.png";
+import { Link, Outlet } from "react-router-dom";
 
 const ResumeTemplate = () => {
+
   return (
     <div className="pt-32 pb-8 bg-[#f4f7f8]">
       {/* menu section */}
-      <div class="border md:w-1/2 border-blue-300 md:rounded-full mx-auto">
+      <div style={{width:"350px"}} class="border border-accent rounded-full mx-auto">
         <div class="">
-          <ul class="flex flex-col md:flex-row justify-around p-4 md:py-1">
-            <li className="py-2 px-4 cursor-pointer hover:bg-primary hover:text-white font-bold rounded-full text-primary text-center">
-              <a>Resume Templates</a>
-            </li>
-            <li className="py-2 px-4 cursor-pointer hover:bg-primary hover:text-white font-bold rounded-full text-primary text-center">
-              <a>CV Templates</a>
-            </li>
-            <li className="py-2 px-4 cursor-pointer hover:bg-primary hover:text-white font-bold rounded-full text-primary text-center">
-              <a>Cover Letter Templates</a>
-            </li>
+          <ul class="flex flex-row justify-between md:px-1 md:py-1">
+            <Link className=" text-lg hover:bg-accent hover:text-white px-6 py-1 rounded-full" to="/">Resume</Link>
+            <Link className=" text-lg hover:bg-accent hover:text-white px-6 py-1 rounded-full" to="/cv">CV</Link>
+            <Link className=" text-lg hover:bg-accent hover:text-white px-6 py-1 rounded-full" to="/coverLetter">Cover Letter</Link>
           </ul>
         </div>
       </div>
       {/* template section */}
-      <div className="mx-12 my-12 ">
-        <div className="grid grid-cols-1 md:grid-cols-4 gap-8">
-          <div
-            style={{ letterSpacing: "3px" }}
-            className="hover:drop-shadow-[0_20px_15px_rgba(65,105,144,.6)]"
-          >
-            <h1 className="mb-4 text-md text-primary mt-1">Professional</h1>
-            <img className="w-full" src={template_1} alt="" />
-          </div>
-          <div
-            style={{ letterSpacing: "3px" }}
-            className="hover:drop-shadow-[0_20px_15px_rgba(65,105,144,.6)]"
-          >
-            <h1 className="mb-4 text-md text-primary mt-1">Professional</h1>
-            <img className="w-full" src={template_2} alt="" />
-          </div>
-          <div
-            style={{ letterSpacing: "3px" }}
-            className="hover:drop-shadow-[0_20px_15px_rgba(65,105,144,.6)]"
-          >
-            <h1 className="mb-4 text-md text-primary mt-1">Professional</h1>
-            <img className="w-full" src={template_1} alt="" />
-          </div>
-          <div
-            style={{ letterSpacing: "3px" }}
-            className="hover:drop-shadow-[0_20px_15px_rgba(65,105,144,.6)]"
-          >
-            <h1 className="mb-4 text-md text-primary mt-1">Professional</h1>
-            <img className="w-full" src={template_2} alt="" />
-          </div>
-          <div
-            style={{ letterSpacing: "3px" }}
-            className="hover:drop-shadow-[0_20px_15px_rgba(65,105,144,.6)]"
-          >
-            <h1 className="mb-4 text-md text-primary mt-1">Professional</h1>
-            <img className="w-full" src={template_1} alt="" />
-          </div>
-          <div
-            style={{ letterSpacing: "3px" }}
-            className="hover:drop-shadow-[0_20px_15px_rgba(65,105,144,.6)]"
-          >
-            <h1 className="mb-4 text-md text-primary mt-1">Professional</h1>
-            <img className="w-full" src={template_2} alt="" />
-          </div>
-          <div
-            style={{ letterSpacing: "3px" }}
-            className="hover:drop-shadow-[0_20px_15px_rgba(65,105,144,.6)]"
-          >
-            <h1 className="mb-4 text-md text-primary mt-1">Professional</h1>
-            <img className="w-full" src={template_1} alt="" />
-          </div>
-          <div
-            style={{ letterSpacing: "3px" }}
-            className="hover:drop-shadow-[0_20px_15px_rgba(65,105,144,.6)]"
-          >
-            <h1 className="mb-4 text-md text-primary mt-1">Professional</h1>
-            <img className="w-full" src={template_2} alt="" />
-          </div>
-        </div>
-      </div>
+      <Outlet/>
     </div>
   );
 };
