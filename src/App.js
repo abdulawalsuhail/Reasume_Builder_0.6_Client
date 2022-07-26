ff0e29f20554fb4131d2506e0ded78a5dbba7381
ifty03 <ashikulislamb84@gmail.com>
2022-07-26 19:51:33 +0600

    modify component name

--- src/App.js
+++ src/App.js
@@ -10,10 +10,10 @@ import Interview from "./Pages/CarrerCounceling/Interview";
 import PersonalDev from "./Pages/CarrerCounceling/PersonalDev";
 import Stories from "./Pages/CarrerCounceling/Stories";
 import Home from "./Pages/Home/Home";
-import CoverLetter from "./Pages/Home/ResumeTemplate/CoverLetter";
-import Cv from "./Pages/Home/ResumeTemplate/Cv";
-import Resume from "./Pages/Home/ResumeTemplate/Resume";
-import ResumeTemplate from "./Pages/Home/ResumeTemplate/ResumeTemplate";
+import CoverLetter from "./Pages/Home/DemoTemplate/CoverLetter";
+import Cv from "./Pages/Home/DemoTemplate/Cv";
+import Resume from "./Pages/Home/DemoTemplate/Resume";
+import DemoTemplate from "./Pages/Home/DemoTemplate/DemoTemplate";
 import Footer from "./Shared/Footer/Footer";
 import CarrerResume from "./Pages/CarrerCounceling/CarrerResume";
 import CarrerCv from "./Pages/CarrerCounceling/CarrerCv";
@@ -21,28 +21,27 @@ import CarrerCv from "./Pages/CarrerCounceling/CarrerCv";
 function App() {
   return (
     <div>
-  
       <Routes>
         <Route path="/" element={<Home />}>
           <Route index element={<Resume />}></Route>
           <Route path="cv" element={<Cv />}></Route>
-          <Route path="coverLetter" element={<CoverLetter/>}></Route>
+          <Route path="coverLetter" element={<CoverLetter />}></Route>
         </Route>
         <Route path="/home" element={<Home />}></Route>
         <Route path="/signup" element={<Signup></Signup>}></Route>
         <Route path="/login" element={<Login />}></Route>
 
         {/* nested route career counselling */}
-        <Route path="/career-counselling" element={<CarrerHeader/>}>
-          <Route index element={<AllArticle/>}></Route>
-          <Route path="resume-write" element={<CarrerResume/>}></Route>
-          <Route path="cv-write" element={<CarrerCv/>}></Route>
-          <Route path="examples" element={<Example/>}/>
-          <Route path="personal-development" element={<PersonalDev/>}/>
-          <Route path="inspiring-stories" element={<Stories/>}/>
-          <Route path="interview-preparation" element={<Interview/>}/>
+        <Route path="/career-counselling" element={<CarrerHeader />}>
+          <Route index element={<AllArticle />}></Route>
+          <Route path="resume-write" element={<CarrerResume />}></Route>
+          <Route path="cv-write" element={<CarrerCv />}></Route>
+          <Route path="examples" element={<Example />} />
+          <Route path="personal-development" element={<PersonalDev />} />
+          <Route path="inspiring-stories" element={<Stories />} />
+          <Route path="interview-preparation" element={<Interview />} />
         </Route>
-        <Route path="resumeTemplate" element={<ResumeTemplate />} />
+        <Route path="resumeTemplate" element={<DemoTemplate />} />
       </Routes>
       <Footer />
       <Toaster />
