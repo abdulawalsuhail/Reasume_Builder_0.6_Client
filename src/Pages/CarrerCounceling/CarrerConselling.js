7ded25e86de540fcd95b12198636433c6cdee14b
shariarhossain23e <shariarhossain23@gamil.comm>
2022-07-26 19:56:18 +0600

    counselling pay

--- src/Pages/CarrerCounceling/CarrerConselling.js
+++ src/Pages/CarrerCounceling/CarrerConselling.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import CouncellingBanner from './CouncellingBanner';
+import CounsellingServices from './CounsellingServices';
 
 const CarrerConselling = () => {
     return (
         <div>
           <CouncellingBanner/>
+          <CounsellingServices/>
         </div>
     );
 };
