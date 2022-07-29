a8e3c82b46678778f17f2150f7951dd495d0c5c3
abdulawalsuhail <abdullahsuhail43@gmail.com>
2022-07-29 14:45:30 +0600

    all ok

--- src/Pages/Home/WriteResume/WriteResume.js
+++ src/Pages/Home/WriteResume/WriteResume.js
@@ -7,26 +7,14 @@ const WriteResume = () => {
     <div className="grid md:grid-cols-2 gap-10 content-center md:px-10 lg:px-20 bg-[#f4f7f8] py-10">
       <div className="flex items-center md:px-0 lg-px-0 px-5 md:order-1 order-2">
         <div className="lg:max-w-md">
-          <h2
-            data-aos="fade-up"
-            data-aos-duration="2000"
-            className="text-5xl font-semibold text-gray-800 front-crimson"
-          >
+          <h2 className="text-5xl font-semibold text-gray-800 front-crimson">
             The Right Resume & CV Maker for the Job
           </h2>
-          <p
-            data-aos="fade-up"
-            data-aos-duration="2000"
-            className="text-lg lg:my-10 my-5 text-gray-600"
-          >
+          <p className="text-lg lg:my-10 my-5 text-gray-600">
             Tech, Engineering, Management or Marketing - CV generator with the
             right resume templates to highlight your career journey.
           </p>
-          <button
-            data-aos="fade-up"
-            data-aos-duration="3000"
-            className="btn btn-primary text-white"
-          >
+          <button className="btn btn-primary text-white">
             Explore all Templates
           </button>
         </div>
