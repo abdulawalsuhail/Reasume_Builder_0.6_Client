76a3145bdf2c4135371b14a4e0e280a02274bb2a
shariarhossain23e <shariarhossain23@gamil.comm>
2022-08-01 18:16:22 +0600

    added navbar

--- src/Pages/EditorComponetn/CommonEditorComponent/EditorNavbar.js
+++ src/Pages/EditorComponetn/CommonEditorComponent/EditorNavbar.js
@@ -1,20 +1,65 @@
-import React from 'react';
+import { signOut } from 'firebase/auth';
+import React, { useState } from 'react';
+import toast from "react-hot-toast";
+import { AiOutlineMenu } from "react-icons/ai";
+import { Link } from "react-router-dom";
+import logo from "../../../assets/logo.png";
+import auth from '../../../firebase.init';
 
 const EditorNavbar = () => {
+    const [toggle,setToggle] = useState(false)
+    console.log(toggle);
     return (
         <div class="navbar bg-base-100">
         <div class="flex-none">
-          <button class="btn btn-square btn-ghost">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-5 h-5 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
-          </button>
+        <label
+            for="editor-nav"
+            class=" drawer-button lg:hidden"
+          >
+            <AiOutlineMenu onClick={() => setToggle(!false)} className='text-4xl'/>
+          </label>
         </div>
         <div class="flex-1">
-          <a class="btn btn-ghost normal-case text-xl">daisyUI</a>
+        <Link to="/">
+            <div className="flex items-center">
+              <img className="w-14" src={logo} alt="logo" />
+
+              <div className="flex gap-2 text-xl">
+                <p>Resume</p>
+                <p>Builder</p>
+              </div>
+            </div>
+          </Link>
         </div>
         <div class="flex-none">
-          <button class="btn btn-square btn-ghost">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-5 h-5 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"></path></svg>
-          </button>
+        <div class="dropdown dropdown-end ml-2">
+              <label tabindex="0" class="btn btn-ghost btn-circle avatar">
+                <div class="w-10 rounded-full">
+                  <img src="https://placeimg.com/80/80/people" />
+                </div>
+              </label>
+              <ul
+                tabindex="0"
+                class="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52"
+              >
+                <li>
+                  <a class="justify-between">
+                    Profile
+                    <span class="badge">New</span>
+                  </a>
+                </li>
+                <li>
+                  <p
+                    onClick={async () => {
+                      await signOut(auth);
+                      toast.success("Sign Out Successfully");
+                    }}
+                  >
+                    Sign Out
+                  </p>
+                </li>
+              </ul>
+            </div>
         </div>
       </div>
     );
