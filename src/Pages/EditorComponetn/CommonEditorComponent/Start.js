5e06bee7a4613e3e4ffe97a4e6e96249219b6d8a
Sadnan-2019 <54290526+Sadnan-2019@users.noreply.github.com>
2022-08-02 16:19:14 +0600

    puul new

--- src/Pages/EditorComponetn/CommonEditorComponent/Start.js
+++ src/Pages/EditorComponetn/CommonEditorComponent/Start.js
@@ -2,6 +2,7 @@ import React from "react";
 import EditorNavbar from "./EditorNavbar";
 import imgtwo from "../../../assets/start2.png";
 import imgone from "../../../assets/start1.png";
+import { Link } from "react-router-dom";
 
 const Start = () => {
   return (
@@ -13,6 +14,7 @@ const Start = () => {
         How would you like to start?
       </h1>
       <div className=" flex    flex-col md:flex-row   justify-center items-center gap-5 	 py-8  ">
+        <Link to="">
         <div class=" card   border-2 rounded-xl border-red-500    shadow-2xl bg-base-100 hover:drop-shadow-[0_20px_15px_rgba(65,105,144,.6)]   ">
           <img src={imgtwo} className="w-[500px] " alt="Shoes" />
 
@@ -24,7 +26,9 @@ const Start = () => {
             We'll guide you through each resume section!
           </p>
         </div>
-        <div class=" card    shadow-2xl bg-base-100  hover:drop-shadow-[0_20px_15px_rgba(65,105,144,.6)] hover:border-2 rounded-1xl border-red-500   ">
+        </Link>
+      <Link>
+      <div class=" card    shadow-2xl bg-base-100  hover:drop-shadow-[0_20px_15px_rgba(65,105,144,.6)] hover:border-2 rounded-1xl border-red-500   ">
           <img src={imgone} className="w-[500px] " alt="Shoes" />
 
           <h2 class=" text-3xl text-center text-extrabold mt-5">
@@ -35,6 +39,7 @@ const Start = () => {
             We'll guide you through each resume section!
           </p>
         </div>
+      </Link>
       </div>
     </div>
     </div>
