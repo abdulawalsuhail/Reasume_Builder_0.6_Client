1ece8da97c8e7d295dcc2fc40ff9d4f9667a131e
ifty03 <ashikulislamb84@gmail.com>
2022-08-11 13:27:18 +0600

    cover letter navbar added

--- src/Pages/CoverLatterTemplate/CoverLetter/HowWriteCoverLetter.js
+++ src/Pages/CoverLatterTemplate/CoverLetter/HowWriteCoverLetter.js
@@ -2,13 +2,20 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import unique from "../../../assets/unique.png";
 import example from "../../../assets/example.png";
+import CoverLetterNavbar from "./CoverLetterNavbar";
 
 const HowWriteCoverLetter = () => {
   const navigate = useNavigate();
   return (
     <div className="h-min-screen bg-[#f4f7f8a5]">
-      <div className="px-8">
-        <h1 className="md:text-5xl text-4xl front-crimson font-semibold pt-20 md:text-center">
+      <CoverLetterNavbar />
+      <div
+        className="px-8"
+        data-aos="fade-down"
+        data-aos-easing="linear"
+        data-aos-duration="1500"
+      >
+        <h1 className="md:text-5xl text-4xl front-crimson font-semibold lg:pt-20 md:pt-14 pt-10 md:text-center">
           How would you write your letter?
         </h1>
         <p className="text-lg text-gray-500 md:text-center py-4">
@@ -18,6 +25,8 @@ const HowWriteCoverLetter = () => {
       </div>
       <div className=" flex flex-col md:flex-row justify-center items-center gap-6 py-8  ">
         <div
+          data-aos="zoom-in-right"
+          data-aos-duration="2000"
           onClick={() => navigate("/resume-builder/editor-contact")}
           class=" card rounded-xl hover:shadow-2xl bg-base-100 border-[1px] border-green-500 w-[280px] h-[240px] cursor-pointer p-8"
         >
@@ -28,6 +37,8 @@ const HowWriteCoverLetter = () => {
           </h2>
         </div>
         <div
+          data-aos="zoom-in-left"
+          data-aos-duration="2000"
           onClick={() => navigate("/resume-builder/drag-and-drop-file")}
           class=" card hover:shadow-2xl bg-base-100  rounded-1xl border-[1px] border-gray-300 hover:border-green-500 w-[280px] h-[240px] cursor-pointergit p-8 cursor-pointer"
         >
