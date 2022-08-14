4982c9e72a2eaf328a761c403cd424ae754500c0
Md.Abu Tahir Sarker <msarker141151@bscse.uiu.ac.bd>
2022-08-15 02:33:45 +0600

    cv and coverLetter route done

--- src/Pages/Home/Templates/CoverLetter.js
+++ src/Pages/Home/Templates/CoverLetter.js
@@ -1,45 +1,110 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import template_1 from "../../../assets/demo1.png";
-import template_2 from "../../../assets/demo2.png";
+import Slider from "react-slick";
+import { AiFillStar } from "react-icons/ai";
 
 const CoverLetter = () => {
-    const coverLetter_Templates = [
+    const cover_Letter_Templates = [
         {
             id: 1,
             name: 'Template_1',
-            image:template_2
+            image: "https://i.ibb.co/7YL4sSM/cl1.png"
         },
         {
             id: 2,
             name: 'Template_2',
-            image:template_1
+            image: "https://i.ibb.co/BgxpcSp/cl9.png"
         },
         {
             id: 3,
-            name: 'Template_1',
-            image:template_2
+            name: 'Template_3',
+            image: "https://i.ibb.co/znT2rPr/cl8.png"
         },
         {
             id: 4,
-            name: 'Template_2',
-            image:template_1
+            name: 'Template_4',
+            image: "https://i.ibb.co/ph53hc7/cl7.png"
         },
+        {
+            id: 5,
+            name: 'Template_5',
+            image: "https://i.ibb.co/rdS7G57/cl5.png"
+        },
+        {
+            id: 6,
+            name: 'Template_6',
+            image: "https://i.ibb.co/7JpW2TC/cl4.png"
+        }
+        
     ]
+
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        initialSlide: 0,
+        autoplay: true,
+        autoplaySpeed: 3000,
+      responsive: [
+        {
+          breakpoint: 1025,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            initialSlide: 1
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
+    };
+    
     return (
-        <>
+        <Slider {...settings}>
             {
-                coverLetter_Templates.map(tem => {
-                return <div
-                    style={{ letterSpacing: "3px", position:"relative"  }}
-                    className="customDiv hover:drop-shadow-[0_20px_15px_rgba(65,105,144,.6)]">
-                    <h1 className="mb-2 text-md text-accent mt-1">{tem.name}</h1>
-                    <img className="w-full" src={tem.image} alt="" />
-                    <Link to="/resume-builder/how-to-start"><a href='#' style={{position:"absolute", left:"20%", right:"20%", bottom:"50%"}} className='customButton  border p-2 border-0 hidden bg-accent text-white'>Edit Template</a></Link>
-                </div>
+                cover_Letter_Templates.map(temp => {
+                    return (
+                        <div
+                            style={{ letterSpacing: "3px" }}
+                            className=' md:px-4 lg:px-6 templateContainer'>
+                            <img style={{ width: "100%" }} className="templateImage" src={temp.image} alt="" />
+                            <div className='w-[100%] md:w-[91%] lg:w-[89%] templateButton p-10'>
+                                <div className='w-[90%] mx-auto flex flex-col justify-between'>
+                                    <Link className='customButton text-center' to="/resume-builder/how-to-start" style={{"--clr":"#6eff3e"}}><span>Edit Template</span><i></i></Link>
+                                    <Link className='customButton text-center' to="/resume-builder/how-to-start" style={{"--clr":"#ff1867"}}><span>Preview</span><i></i></Link>
+                                </div>
+                                <div className='text-center'>
+                                    <h1 className='text-2xl'>Our client satisfaction?</h1>
+                                    <div className='flex mt-1 justify-center'>
+                                        <AiFillStar className='text-green-800 text-xl'/>
+                                        <AiFillStar className='text-green-800 text-xl'/>
+                                        <AiFillStar className='text-green-800 text-xl'/>
+                                        <AiFillStar className='text-green-800 text-xl'/>
+                                        <AiFillStar className='text-green-800 text-xl'/>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    )
                 })
             }
-         </>
+        </Slider>
     );
 };
 
