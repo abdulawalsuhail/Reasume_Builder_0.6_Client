38f8deee673136fa2383590f450e1175592d1dc3
shariarhossain23e <shariarhossain23@gamil.comm>
2022-08-15 22:33:48 +0600

    navbar plm solved

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -120,9 +120,9 @@ const Navbar = () => {
           </div>
           <Link to="/">
             <div className="flex items-center">
-              <img className="w-14 mt-2" src={logo} alt="logo" />
+              <img className="w-14" src={logo} alt="logo" />
 
-              <div className="text-[15px]">
+              <div className="flex gap-2 text-[20px]">
                 <p>Resume</p>
                 <p>Builder</p>
               </div>
