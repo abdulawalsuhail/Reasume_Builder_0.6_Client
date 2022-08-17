07cc089b24589caa37927ef20c26445ae5b69903
shariarhossain23e <shariarhossain23@gamil.comm>
2022-08-17 22:28:54 +0600

    see card blog and conditionaly change

--- src/Pages/Dashboard/MyBlog/MyblogCard.js
+++ src/Pages/Dashboard/MyBlog/MyblogCard.js
@@ -1,18 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import { AiFillDelete } from "react-icons/ai";
 import { BiEdit } from "react-icons/bi";
-import { Link, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 const MyblogCard = ({ blog, setBlogId }) => {
   const { blogType, title, blog: text, _id } = blog;
+  const [isOpen,setOpen] = useState(false)
   const navigate = useNavigate();
+
   return (
     <div>
-      <div class="card max-w-lg h-[250px] overflow-x-hidden bg-base-100 shadow-xl mt-4">
+      <div class="card max-w-sm h-[100%] bg-base-100 shadow-xl mt-4">
         <div class="card-body">
           <h2 className="text-xl font-bold">{blogType}</h2>
           <h2 class="card-title">{title}</h2>
-          <p>{text.length > 100 ? <span>{text.slice(0,100)} <Link className="text-[#3f66da] font-bold" to='dashboard/see details'>See more ...</Link></span> :text}</p>
+          <p className="text-gray-500">
+            {text.length > 100 ? <span>{text.slice(0,100) }{
+              isOpen && <span>{text.slice(100,text.length)}</span>
+            }
+             <span onClick={()=>setOpen(!isOpen)} className="text-[#3f66da] font-bold">{isOpen ? "see less" :"see more..."}   </span>   </span>
+              :text}
+              </p>
           <div class="card-actions justify-end">
             <BiEdit onClick={() => navigate(`/dashboard/edit-blog-post/${_id}`)} className="text-2xl text-[#f7a000] mr-2" />
             <label onClick={() => setBlogId(_id)} for="blog-delete-modal">
