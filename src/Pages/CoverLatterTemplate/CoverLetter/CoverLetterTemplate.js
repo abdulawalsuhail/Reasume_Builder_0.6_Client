537e3abe6812ba72ea972c06be9378c53348d4ea
ifty03 <ashikulislamb84@gmail.com>
2022-08-19 20:29:53 +0600

    cover leter editable page

--- src/Pages/CoverLatterTemplate/CoverLetter/CoverLetterTemplate.js
+++ src/Pages/CoverLatterTemplate/CoverLetter/CoverLetterTemplate.js
@@ -48,25 +48,36 @@ const CoverLetterTemplate = () => {
       </div>
       <div className="w-[65%] mx-auto bg-white  ">
         <div ref={componentRef} className="p-16">
-          <h2 className="text-3xl poppins-r text-gray-700">{name}</h2>
-          <h4 className="text-lg poppins-r text-gray-600">{jobRole}</h4>
-          <div className="flex gap-5 my-4 text-[13px] poppins-t">
-            <p className="flex gap-1 place-items-center">
-              <HiPhone /> {phone}
-            </p>
-            <p className="flex gap-1 place-items-center">
-              <MdOutlineMail /> {email}
-            </p>
-            <p className="flex gap-1 place-items-center">
-              <TbBrandLinkedin /> {linkedin}
-            </p>
+          <div
+            contentEditable
+            className="p-3 border-0 focus:border-[1px] outline-none border-green-600 rounded-md focus:bg-gray-100"
+          >
+            <h2 className="text-3xl poppins-r text-gray-700">{name}</h2>
+            <h4 className="text-lg poppins-r text-gray-600">{jobRole}</h4>
+            <div className="flex gap-5 my-2 text-[13px] poppins-t">
+              <p className="flex gap-1 place-items-center">
+                <HiPhone /> {phone}
+              </p>
+              <p className="flex gap-1 place-items-center">
+                <MdOutlineMail /> {email}
+              </p>
+              <p className="flex gap-1 place-items-center">
+                <TbBrandLinkedin /> {linkedin}
+              </p>
+            </div>
           </div>
           <div className="text-[15px] poppins-t text-gray-600">
-            <p className="mb-4">
+            <p
+              contentEditable
+              className="p-3 mb-2 border-0 focus:border-[1px] outline-none border-green-600 rounded-md focus:bg-gray-100"
+            >
               Dear <span contentEditable>[Hiring Manager's Name]</span>,
             </p>
 
-            <p className="mb-4">
+            <p
+              contentEditable
+              className="p-3 mb-2 border-0 focus:border-[1px] outline-none border-green-600 rounded-md focus:bg-gray-100"
+            >
               You must be receiving floods of resumes for the role of {jobRole}.
               So, I will cut short. I believe I would be a great fit for the{" "}
               {jobRole}
@@ -74,13 +85,19 @@ const CoverLetterTemplate = () => {
               pull this off.
             </p>
 
-            <p className="mb-3">
+            <p
+              contentEditable
+              className="p-3 mb-2 border-0 focus:border-[1px] outline-none border-green-600 rounded-md focus:bg-gray-100"
+            >
               I've recently finished my internship at [Company Name] [Department
               Name]. This experience was incredibly valuable, as it helped me
               kick off my career in {jobRole} in three different fields:
             </p>
 
-            <ul class="list-disc pl-4 pb-3">
+            <ul
+              contentEditable
+              className="p-3 mb-2 border-0 focus:border-[1px] outline-none border-green-600 rounded-md focus:bg-gray-100"
+            >
               <li>
                 I've further developed my {skillOne} and {skillTwo} I learned
                 during my [Education field] at [University Name].
@@ -95,32 +112,49 @@ const CoverLetterTemplate = () => {
               </li>
             </ul>
 
-            <p className="mb-4">
+            <p
+              contentEditable
+              className="p-3 mb-2 border-0 focus:border-[1px] outline-none border-green-600 rounded-md focus:bg-gray-100"
+            >
               The reason why I consider the above a defining experience for me
               is the results I was able to achieve. skill three helped me to
               [improvement] by [30%].
             </p>
 
-            <p>
+            <p
+              contentEditable
+              className="p-3 border-0 focus:border-[1px] outline-none border-green-600 rounded-md focus:bg-gray-100"
+            >
               Additionally, my work at [Previous Company Name] helped me gain
               first-hand experience in {strengthOne}, {strengthTwo} and{" "}
               {strengthThree}.
             </p>
-            <p className="my-4">
+            <p
+              contentEditable
+              className="p-3 my-2 border-0 focus:border-[1px] outline-none border-green-600 rounded-md focus:bg-gray-100"
+            >
               Above all requirements, I believe you are looking for a capable
               person who can address challenging tasks such as Biggest Chalange.
               I can fulfil this expectation, because {challenge}
             </p>
 
-            <p className="mb-4">
+            <p
+              contentEditable
+              className="p-3 mb-2 border-0 focus:border-[1px] outline-none border-green-600 rounded-md focus:bg-gray-100"
+            >
               Could we grab a coffee and have a chat about how do you see a next
               job develop into an integral part of the [Company Name]'s team?
             </p>
             <div>
-              <p>Best Regards,</p>
-              <p>
-                {name}, {jobRole}
-              </p>
+              <div
+                contentEditable
+                className="p-3 mb-2 border-0 focus:border-[1px] outline-none border-green-600 rounded-md focus:bg-gray-100"
+              >
+                <p>Best Regards,</p>
+                <p>
+                  {name}, {jobRole}
+                </p>
+              </div>
             </div>
           </div>
         </div>
