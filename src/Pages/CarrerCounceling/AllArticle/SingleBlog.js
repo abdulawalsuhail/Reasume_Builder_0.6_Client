bd20dc6a1bd23bfab13c09fd9cd1b8dce56a7120
shariarhossain23e <shariarhossain23@gamil.comm>
2022-08-21 00:11:20 +0600

    single card

--- src/Pages/CarrerCounceling/AllArticle/SingleBlog.js
+++ src/Pages/CarrerCounceling/AllArticle/SingleBlog.js
@@ -1,30 +1,37 @@
 import React from "react";
-import { FaFacebook, FaLinkedinIn } from 'react-icons/fa';
+import { FaFacebook, FaLinkedinIn } from "react-icons/fa";
 import { useParams } from "react-router-dom";
+import logo from '../../../assets/icon/profile.png';
 import BlogQuery from "../../../Hook/BlogQuery";
-
 
 const SingleBlog = () => {
   const { id } = useParams();
-  
-  const [blog] = BlogQuery(id);
 
+  const [blog] = BlogQuery(id);
 
   return (
     <div class="card md:w-[70%] mx-auto h-screen bg-base-100 shadow-xl">
       <div class="card-body">
-        <h2 class="text-[25px] font-bold">{blog.title}</h2>
-        <div className="flex justify-between mt-10">
-            <div>
-                <h1 className="text-[18px] text-gray-500 font-bold capitalize">{blog.name}</h1>
-                <h1 className="text-[15px] text-gray-500">{blog.date}</h1>
-            </div>
-            <div className="flex gap-2">
-                <FaFacebook className="text-2xl text-gray-500"/>
-                <FaLinkedinIn className="text-2xl text-gray-500"/>
-            </div>
+        <div class="avatar">
+          <div class="w-24 mx-auto rounded-full ring ring-offset-base-100 ring-offset-2">
+            {
+              blog?.img?<img src={blog?.img} />:<img src={logo} />
+            }
+          </div>
         </div>
+        <div>
+          <h1 className="text-[18px] mt-4 text-center text-gray-500 font-bold capitalize">
+            {blog.name}
+          </h1>
+          <h1 className="text-[15px] text-gray-500 text-center">{blog.date}</h1>
+        </div>
+        <h2 class="text-[25px] font-bold mt-4">{blog.title}</h2>
+
         <p className="text-[22px] text-gray-500 mt-10">{blog.blog}</p>
+          <div className="flex gap-2 justify-end">
+            <FaFacebook className="text-2xl text-gray-500" />
+            <FaLinkedinIn className="text-2xl text-gray-500" />
+          </div>
       </div>
     </div>
   );
