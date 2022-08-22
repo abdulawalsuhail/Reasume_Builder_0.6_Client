b556dc3becd4a369a2f1a6313e46cf32d979e229
ifty03 <ashikulislamb84@gmail.com>
2022-08-22 12:38:40 +0600

    service section added

--- src/Pages/Home/WriteResume/WriteResume.js
+++ src/Pages/Home/WriteResume/WriteResume.js
@@ -4,7 +4,7 @@ import "./WriteResume.css";
 
 const WriteResume = () => {
   return (
-    <div className="grid md:grid-cols-2 gap-7 content-center md:mx-16 mx-8 py-10">
+    <div className="grid md:grid-cols-2 gap-7 lg:gap-14 content-center md:mx-16 lg:mx-24 mx-8 py-10">
       <div className="flex items-center md:order-1 order-2">
         <div className="lg:max-w-md">
           <h2 className="text-4xl md:text-4xl lg:text-5xl font-semibold text-gray-800 front-crimson">
