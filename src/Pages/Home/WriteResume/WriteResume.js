6173ea82c995ac659b2fa7ed3bec1763d0129593
ifty03 <ashikulislamb84@gmail.com>
2022-08-22 10:47:48 +0600

    modify write resume section

--- src/Pages/Home/WriteResume/WriteResume.js
+++ src/Pages/Home/WriteResume/WriteResume.js
@@ -4,10 +4,10 @@ import "./WriteResume.css";
 
 const WriteResume = () => {
   return (
-    <div className="grid md:grid-cols-2 gap-10 content-center md:px-10 lg:px-20 bg-[#f4f7f8] py-10">
-      <div className="flex items-center md:px-0 lg-px-0 px-5 md:order-1 order-2">
+    <div className="grid md:grid-cols-2 gap-7 content-center md:mx-16 mx-8 py-10">
+      <div className="flex items-center md:order-1 order-2">
         <div className="lg:max-w-md">
-          <h2 className="text-5xl font-semibold text-gray-800 front-crimson">
+          <h2 className="text-4xl md:text-4xl lg:text-5xl font-semibold text-gray-800 front-crimson">
             The Right Resume & CV Maker for the Job
           </h2>
           <p className="text-lg lg:my-10 my-5 text-gray-600">
