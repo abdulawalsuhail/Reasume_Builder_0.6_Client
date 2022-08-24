6900911407ada91d7e4007c99548bbddcf9effdb
ifty03 <ashikulislamb84@gmail.com>
2022-08-24 12:03:37 +0600

    quiz start btn added

---
+++ src/Pages/Quiz/QuizStart.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const QuizStart = () => {
+  return (
+    <div className="flex justify-center items-center max-h-screen bg-green-50">
+      <button className="btn btn-success">Start Quiz</button>
+    </div>
+  );
+};
+
+export default QuizStart;
