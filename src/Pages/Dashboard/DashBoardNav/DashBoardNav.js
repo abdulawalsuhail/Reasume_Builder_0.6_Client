2293806937b51220620378059f1543b230318d22
shariarhossain23e <shariarhossain23@gamil.comm>
2022-08-26 19:35:38 +0600

    fixed bug

--- src/Pages/Dashboard/DashBoardNav/DashBoardNav.js
+++ src/Pages/Dashboard/DashBoardNav/DashBoardNav.js
@@ -1,5 +1,5 @@
 import { signOut } from "firebase/auth";
-import React from "react";
+import React, { useEffect } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
 import toast from "react-hot-toast";
 import { AiOutlineMenu } from "react-icons/ai";
@@ -8,10 +8,20 @@ import profile from '../../../assets/icon/profile.png';
 import logo from "../../../assets/logo.png";
 import auth from "../../../firebase.init";
 import UserInformation from "../../../Hook/UserInformation";
+import Loading from "../../../Shared/Loading/Loading";
 
 const DashBoardNav = () => {
     const [user] = useAuthState(auth)
-    const [users] = UserInformation(user)
+    const [users,isLoading,refetch] = UserInformation(user)
+
+    useEffect(()=>{
+        refetch()
+    },[users])
+
+    if(isLoading){
+        return <Loading/>
+    }
+
   return (
     <div>
       <div class="navbar bg-base-100 px-12 bg-[#f4f7f8] sticky top-0 z-50 ">
